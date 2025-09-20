83556a30d6577c1285cb9b5fb999f55b0376ab5a
ccgargantua <ccgargantua@proton.me>
2025-09-20 08:36:42 -0400

    Addition of simple testing

--- systems-template/src/example_compilation_unit.c
+++ systems-template/src/example_compilation_unit.c
@@ -1,1 +1,17 @@
 #include "example_compilation_unit.h"
+
+Vector2i add_vector2i(Vector2i a, Vector2i b)
+{
+    return (Vector2i){a.x + b.x, a.y + b.y};
+}
+
+#include <rktest.h>
+
+TEST(example_tests, add_vector2i_test)
+{
+    Vector2i v1 = {1, 2};
+    Vector2i v2 = {3, 4};
+    Vector2i v3 = add_vector2i(v1, v2);
+    EXPECT_EQ(v3.x, v1.x + v2.x);
+    EXPECT_EQ(v3.y, v1.y + v2.y);
+}
--- systems-template/src/example_compilation_unit.h
+++ systems-template/src/example_compilation_unit.h
@@ -1,4 +1,12 @@
 #ifndef EXAMPLE_COMPILATION_UNIT_H
 #define EXAMPLE_COMPILATION_UNIT_H
 
+typedef struct
+{
+    int x;
+    int y;
+} Vector2i;
+
+Vector2i add_vector2i(Vector2i a, Vector2i b);
+
 #endif //EXAMPLE_COMPILATION_UNIT_H
